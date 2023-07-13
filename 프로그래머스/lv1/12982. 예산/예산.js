f27db4a8479c40bbd173dfaa654f774c3cb17dc6
junhui324 <122953242+junhui324@users.noreply.github.com>
2023-07-13 12:35:06 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+    let count = 0;
+    d.sort((a, b) => a - b);
+    
+    for(let i = 0; i < d.length; i++) {
+        if(d[i] > budget) {
+            break;
+        }
+        budget -= d[i];
+        count++;
+    }
+    
+    return count;
+}
